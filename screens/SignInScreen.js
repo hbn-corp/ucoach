7e0f22d4de46aa25c07400847030b7fdcd0d9c85
m-naim <mehenaouin@gmail.com>
2019-12-10 11:30:07 +0100

    added screens

---
+++ screens/SignInScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    ScrollView,
+    StyleSheet
+} from 'react-native';
+import {
+    ExpoLinksView
+} from '@expo/samples';
+
+export default function SignInScreen() {
+    return (
+        < ScrollView style={styles.container} >
+            <ExpoLinksView />
+        </ScrollView>
+    );
+}
+
+LinksScreen.navigationOptions = {
+    title: 'Links',
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
+    },
+});
